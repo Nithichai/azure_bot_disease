94351d9f0e8ee87603091a7688a834da835cb9ee
NopyJF <Nithichai@users.noreply.github.com>
2018-02-17 17:13:53 +0700

    Add bot_more.js

---
+++ bot_more.js
@@ -0,0 +1,29 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, [
+    // Headache Test
+    function (session, results) {
+        if (results == 'headache') {
+            session.send('You should be a headache');
+        } else if (results == 'stomachache in right down') {
+            session.send('You should be a Appendicitis');
+        }
+    }
+]);
